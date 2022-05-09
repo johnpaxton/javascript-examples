9bd3ae043f8948c0648bb242db2d33807f3b11eb
John Paxton <johnpaxton@gmail.com>
2022-05-09 00:20:30 -0400

    Remove unnecessary code

--- demos/lowdb-demo/lowdb-node-demo.js
+++
@@ -1,8 +0,0 @@
-const low = require('../../node_modules/lowdb');
-const FileSync = require('../../node_modules/lowdb/adapters/FileSync');
-
-const adapter = new FileSync('../../data/movies.json');
-const db = low(adapter);
-db.defaults({ movies: [] });
-console.log('has: ', db.get('movies').value());
-console.log('Movie 1:', db.get('movies').find({ id: 1 }).value());
