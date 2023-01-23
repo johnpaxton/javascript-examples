160a47424d4e4e8bdab3d99ad939a67539e434aa
John Paxton <johnpaxton@gmail.com>
2023-01-23 14:18:34 -0500

    Somewhat better closure examples

--- demos/functions/closures.js
+++ demos/functions/closures.js
@@ -1,19 +1,64 @@
-function employeeFactory(firstName, lastName) {
-  let salary = 50000;
-  return {
-    firstName,
-    lastName,
-    getSalary() {
-      return salary;
-    },
-    setSalary(s) {
-      s = Number(s);
-      if (s && s >= salary) {
-        salary = s;
-      }
-    },
-  };
+/* eslint-disable no-unused-vars */
+
+let x = 10;
+
+function printX() {
+  console.log(`x is ${x}`);
 }
 
-const emp1 = employeeFactory('John', 'Paxton');
-console.log("employee 1's salary: ", emp1.getSalary());
+printX();
+
+class ClosureOne {
+  instanceVar = 'instanceVar';
+
+  printInstanceVar() {
+    console.log(`instanceVar is ${this.instanceVar}`);
+  }
+}
+
+function outerFunction() {
+  console.log('in outerFunction');
+  let outerValue = 'outerValue';
+  let callbackFunction = function () {
+    console.log('in callbackFunction');
+    console.log(`outerValue is ${outerValue}`);
+  };
+
+  return callbackFunction;
+}
+
+console.log('Calling outerFunction()');
+let cb = outerFunction();
+
+console.log('Calling cb()');
+cb();
+
+// eslint-disable-next-line no-undef
+console.log('Outside cb, trying to access outerValue: ', outerValue);
+
+function privateCounter() {
+  let x = 0;
+
+  let increment = function () {
+    x = x + 1;
+    return x;
+  };
+
+  return increment;
+}
+
+let incrementer = privateCounter();
+let outerX = incrementer();
+outerX = incrementer();
+outerX = incrementer();
+outerX = incrementer();
+console.log(`x is currently ${outerX}`);
+
+class Visibility {
+  x = 10;
+  #y = 20;
+}
+
+let v1 = new Visibility();
+console.log('x=' + v1.x);
+console.log('y=' + v1.y);
