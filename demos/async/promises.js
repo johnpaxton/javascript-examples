dec05656af8558d52b05dbc365bccb4466c537ef
John Paxton <johnpaxton@gmail.com>
2023-01-24 10:37:08 -0500

    Promise examples

--- demos/async/promises.js
+++ demos/async/promises.js
@@ -1,49 +1,91 @@
-const p1 = new Promise((resolve, reject) => {
-  if (Date.now() % 2) {
-    resolve('Success!');
-  } else {
-    // eslint-disable-next-line prefer-promise-reject-errors
-    reject('Failed!');
-  }
-});
-
-function onResolve(result) {
-  console.log('The promise resolved successfully with', result);
+/* eslint-disable no-unused-vars */
+/* eslint-disable n/handle-callback-err */
+/* eslint-disable prefer-promise-reject-errors */
+function generateAPromiseThat(result) {
+  return new Promise((resolve, reject) => {
+    if (result === true || result === 'succeeds') {
+      resolve('Success!');
+    } else if (result === false || result === 'fails') {
+      reject('Failed!');
+    } else {
+      return Date.now() % 2 ? resolve('Random Success!') : reject('Random Failure!');
+    }
+  });
 }
 
-function onReject(result) {
-  console.error('The promise rejected (failed) with', result);
-}
+let initialPromise = generateAPromiseThat('random');
 
-// p1.addEventListener('success', onResolve);
-// p1.addEventListener('error', onReject);
-// p1.then( onResolve );
-p1.then(onResolve, onReject); // check the event queue
+// Promise API: promise.then(successCallback, failureCallback?)
 
-console.log('After .then()'); // check the event queue
+// Success and failure
+initialPromise.then(
+  (result) => console.log(`Resolved with ${result}`),
+  (error) => console.error(`Failed with ${error}`)
+);
 
-const p2 = p1.then((result) => {
-  console.log('result level 1:', result);
-  return 10; // resolve(10)
+// Chained success and failure
+initialPromise
+  .then((success) => console.log('Chained success'))
+  .catch((error) => console.log('Chained fail'));
+
+// Standalone
+generateAPromiseThat('fails').catch((error) => console.error('Something went wrong'));
+generateAPromiseThat('succeeds').then((result) => console.log('Only a success callback'));
+
+let chainedResultsPromise = generateAPromiseThat(true);
+
+let link2Promise = chainedResultsPromise.then((success) => {
+  console.log('Link 1:', success);
+  return 10;
 });
 
-const p3 = p2.then((result) => {
-  console.log('result level 2:', result);
-  return result * 2;
+let link3Promise = link2Promise.then((link2Result) => {
+  console.log('Link 2:', link2Result);
+  return 20;
 });
 
-const x = p3.then((result) => {
-  console.log('result level 3:', result);
+link3Promise.then((link3Result) => {
+  console.log('Link 3:', link3Result);
 });
 
-x.then((someValue) => {
-  // someValue is undefined, because we did not return anything
-  // from the last promise
+generateAPromiseThat('succeeds')
+  .then((link1Result) => {
+    console.log('Chained Link 1:', link1Result);
+    return 10;
+  })
+  .then((link2Result) => {
+    console.log('Chained Link 2:', link2Result);
+    return 20;
+  })
+  .then((link3Result) => {
+    console.log('Chained Link 3:', link3Result);
+  });
+
+generateAPromiseThat('fails')
+  .then((link1Result) => {
+    console.log('Chained Link 1:', link1Result);
+    return 10;
+  })
+  .then((link2Result) => {
+    console.log('Chained Link 2:', link2Result);
+    return 20;
+  })
+  .then((link3Result) => {
+    console.log('Chained Link 3:', link3Result);
+  })
+  .catch((error) => {
+    console.error('Somewhere above, there was a problem:', error);
+  });
+
+let resultsPromise = generateAPromiseThat('succeeds');
+
+// Prefer chaining to capturing a return promise and calling then()
+let results = resultsPromise.then((value) => {
+  return value;
 });
 
-async function whatever() {
-  const result = await p1;
-  console.log(result);
-}
-
-whatever();
+let randomPromise = generateAPromiseThat('who knows');
+randomPromise.finally(() => {
+  console.log('Runs on success OR failure');
+  console.log('But receives no argument');
+});
